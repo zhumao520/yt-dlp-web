eaab5e7b13803baebcc242817bc1f667f572dcec
zhumao <46477115+zhumao520@users.noreply.github.com>
2025-06-19 19:23:22 +0800

    Delete modules directory

--- modules/cookies/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Cookies管理模块
-"""
--- modules/warp/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-WARP 模块 - Cloudflare WARP 集成
-"""
-
-__version__ = "1.0.0"
-__author__ = "YT-DLP Web Team"
-__description__ = "Cloudflare WARP integration for bot detection bypass"
